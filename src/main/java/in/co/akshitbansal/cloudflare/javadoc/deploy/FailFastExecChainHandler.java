2eb756b3eb085e043d8891d4d0c9ebf0531ce519
Akshit Bansal <bansalakshitwork@gmail.com>
2026-03-31 14:33:39 +0530

    Refactor FailFastExecChainHandler to streamline error handling and ensure response closure

--- src/main/java/in/co/akshitbansal/cloudflare/javadoc/deploy/FailFastExecChainHandler.java
+++ src/main/java/in/co/akshitbansal/cloudflare/javadoc/deploy/FailFastExecChainHandler.java
@@ -16,32 +16,26 @@ public class FailFastExecChainHandler implements ExecChainHandler {
     public ClassicHttpResponse execute(ClassicHttpRequest request, ExecChain.Scope scope, ExecChain chain) throws IOException, HttpException {
         log.info("HTTP Request: {} {}", request.getMethod(), request.getRequestUri());
         ClassicHttpResponse response = chain.proceed(request, scope);
+
         int statusCode = response.getCode();
         Header locationHeader = response.getHeader(HttpHeaders.LOCATION);
         String locationHeaderValue = locationHeader != null ? locationHeader.getValue() : null;
+
         if(statusCode >= 300 && statusCode < 400) {
-            try {
-                throw new IllegalStateException(MessageFormat.format(
-                        "Received unexpected redirect response status code: {0}, Location header: {1} while sending HTTP request to {2} {3}",
-                        statusCode, locationHeaderValue, request.getMethod(), request.getRequestUri()
-                ));
-            }
-            finally {
-                // Close the response to prevent connection leaks, as we are not going to follow the redirect
-                response.close();
-            }
+            // Close the response to prevent connection leaks, as we are not going to follow the redirect
+            response.close();
+            throw new IllegalStateException(MessageFormat.format(
+                    "Received unexpected redirect response status code: {0}, Location header: {1} while sending HTTP request to {2} {3}",
+                    statusCode, locationHeaderValue, request.getMethod(), request.getRequestUri()
+            ));
         }
         if(statusCode == 429 || statusCode == 408 || statusCode >= 500) {
-            try {
-                throw new RetryableException(MessageFormat.format(
-                        "Received retryable HTTP response with status code {0} while sending HTTP request to {1} {2}",
-                        statusCode, request.getMethod(), request.getRequestUri()
-                ));
-            }
-            finally {
-                // Close the response to prevent connection leaks, as the request will be retried
-                response.close();
-            }
+            // Close the response to prevent connection leaks, as the request will be retried
+            response.close();
+            throw new RetryableException(MessageFormat.format(
+                    "Received retryable HTTP response with status code {0} while sending HTTP request to {1} {2}",
+                    statusCode, request.getMethod(), request.getRequestUri()
+            ));
         }
         return response;
     }
